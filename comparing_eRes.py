c36c2f81e04f072690b85c4d998b33fda0536d86
jgreen83 <42304272+jgreen83@users.noreply.github.com>
2024-07-11 00:46:33 -0400

    finished comparing diff exp rates for same gamma and beta (tilde)

--- comparing_eRes.py
+++ comparing_eRes.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 trialX1 = np.linspace(0,2,5)
-trialR1 = np.linspace(0,2,5)
+trialR1 = np.linspace(0.01,2,5)
 trialX2 = np.linspace(0,2,5)
 trialR2 = np.linspace(0.01,2,5)
 
 X1, R1 = np.meshgrid(trialX1, trialR1)
 X2, R2 = np.meshgrid(trialX2, trialR2)
 
-PROBS1 = [[1.  , 0.36, 0.28, 0.16, 0.04],
-       [0.76, 0.32, 0.4 , 0.12, 0.04],
-       [0.4 , 0.36, 0.36, 0.2 , 0.04],
-       [0.4 , 0.32, 0.12, 0.12, 0.08],
-       [0.28, 0.12, 0.08, 0.04, 0.  ]]
+PROBS1 = [[1.  , 0.4 , 0.12, 0.2 , 0.04],
+       [0.92, 0.4 , 0.  , 0.12, 0.  ],
+       [0.48, 0.32, 0.08, 0.16, 0.12],
+       [0.32, 0.04, 0.12, 0.08, 0.04],
+       [0.24, 0.08, 0.2 , 0.08, 0.04]]
 PROBS2 = [[1.  , 0.48, 0.4 , 0.28, 0.04],
        [0.96, 0.6 , 0.28, 0.16, 0.08],
        [0.72, 0.36, 0.12, 0.12, 0.08],
